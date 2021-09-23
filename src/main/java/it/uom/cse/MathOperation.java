cf8ce2e8b28d7ce0cc3713f9cda10886608f77a1
Dinuka-Thathsara <dinukathathsara@gmail.com>
2021-09-23 11:37:26 +0530

    Role B

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -14,5 +14,18 @@ public class MathOperation {
     {
         return operand1 - operand2;
     }
+    public static int fib(int n)
+    {
+        int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 
 }
